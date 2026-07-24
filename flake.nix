dac4e5f15b65e212b5785c9a6f81c32b8ae786a6
Aliou Diallo <code@aliou.me>
2026-07-25 01:06:04 +0200

    feat: update to pnpm 11 + lock node version

--- flake.nix
+++ flake.nix
@@ -6,17 +6,24 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
         devShells.default = pkgs.mkShell {
           packages = [
-            pkgs.nodejs
-            pkgs.pnpm
+            pkgs.nodejs_24
+            pkgs.pnpm_11
           ];
         };
-      });
+      }
+    );
 }
